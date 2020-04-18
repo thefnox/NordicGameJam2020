5726b9d6a4a62276d04ec611eb3ee1a3b2c53352
Nikira <qbus89@gmail.com>
2020-04-18 15:34:27 +0200

    updated parent and connector scripts

---
+++ Assets/Scripts/ConnectorScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConnectorScript : MonoBehaviour
+{
+    public bool ConnectorActiveState = false;
+    public Material ConnectedMaterial;
+    public Material DisconnectedMaterial;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (ConnectorActiveState == true)
+        {
+            // Debug.Log("Active state of connector " + ConnectorActiveState);
+            this.gameObject.GetComponent<Renderer>().material = ConnectedMaterial;
+        }
+        else if (true)
+            this.gameObject.GetComponent<Renderer>().material = DisconnectedMaterial;
+    }
+
+    /* private void OnTriggerStay(Collider other)
+    {
+        Debug.Log("Connector Trigger is triggered");
+    }
+    */
+}
